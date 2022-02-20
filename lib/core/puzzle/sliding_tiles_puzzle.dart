dd17213e2c566fcfe707fd3c541a18a45a448474
justshreyas <professionally.shreyas@gmail.com>
2022-02-20 12:49:23 +0530

    TASK : Add core Puzzle logic

--- lib/core/puzzle/sliding_tiles_puzzle.dart
+++ lib/core/puzzle/sliding_tiles_puzzle.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:puzzlehack/core/puzzle/puzzle_grid_position.dart';
 import 'package:puzzlehack/core/puzzle/tile.dart';
 
@@ -6,7 +8,37 @@ class SlidingTilesPuzzle {
   final List<PuzzleTile> tiles;
   final PuzzleGridPosition emptySpace;
 
-  SlidingTilesPuzzle(this.dimension, this.tiles, this.emptySpace);
+  const SlidingTilesPuzzle(this.dimension, this.tiles, this.emptySpace);
+
+  factory SlidingTilesPuzzle.solved(
+    final int dimension,
+  ) {
+    final List<PuzzleTile> tiles = List.generate(
+      dimension * dimension,
+      (index) => PuzzleTile(
+        index.toString(),
+        PuzzleGridPosition(
+          index % dimension,
+          index ~/ dimension,
+        ),
+        PuzzleGridPosition(
+          index % dimension,
+          index ~/ dimension,
+        ),
+      ),
+    );
+    final PuzzleGridPosition emptySpace =
+        PuzzleGridPosition(dimension, dimension);
+
+    return SlidingTilesPuzzle(dimension, tiles, emptySpace);
+  }
+
+  factory SlidingTilesPuzzle.random(
+    final int dimension,
+  ) {
+    final solvedPuzzle = SlidingTilesPuzzle.solved(dimension);
+    return solvedPuzzle.scramble();
+  }
 
   SlidingTilesPuzzle copyWith({
     int? dimension,
@@ -22,6 +54,22 @@ class SlidingTilesPuzzle {
 }
 
 extension SlidingTilesPuzzleX on SlidingTilesPuzzle {
+  SlidingTilesPuzzle scramble() {
+    SlidingTilesPuzzle mutablePuzzle = this;
+    const int numberOfIterations = 1000;
+    final randomizer = Random(numberOfIterations);
+
+    for (var itr = 0; itr < numberOfIterations; itr++) {
+      final int randomIndex = randomizer.nextInt(dimension * dimension);
+      final candidate = mutablePuzzle.tiles.elementAt(randomIndex);
+      if (mutablePuzzle.canMoveTile(candidate)) {
+        mutablePuzzle.moveTile(candidate);
+      }
+    }
+
+    return mutablePuzzle;
+  }
+
   bool get isSolved => tiles.every((t) => t.isInCorrectPosition);
 
   bool canMoveTile(PuzzleTile tile) {
@@ -53,3 +101,5 @@ extension SlidingTilesPuzzleX on SlidingTilesPuzzle {
     );
   }
 }
+
+extension SlidingTilesPuzzleViewX on SlidingTilesPuzzle {}
--- lib/core/puzzle/tile.dart
+++ lib/core/puzzle/tile.dart
@@ -1,16 +1,18 @@
 import 'package:puzzlehack/core/puzzle/puzzle_grid_position.dart';
 
 class PuzzleTile {
+  final String id;
   final PuzzleGridPosition currentPosition;
   final PuzzleGridPosition expectedPosition;
 
-  PuzzleTile(this.currentPosition, this.expectedPosition);
+  PuzzleTile(this.id, this.currentPosition, this.expectedPosition);
 
   PuzzleTile copyWith({
     PuzzleGridPosition? currentPosition,
     PuzzleGridPosition? expectedPosition,
   }) {
     return PuzzleTile(
+      id,
       currentPosition ?? this.currentPosition,
       expectedPosition ?? this.expectedPosition,
     );
