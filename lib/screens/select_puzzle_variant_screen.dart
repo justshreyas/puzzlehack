7917859fa3705db65b7f9095c4f5a672b92f751a
justshreyas <professionally.shreyas@gmail.com>
2022-03-04 19:40:19 +0530

    REFACTOR : Selection Cards

--- lib/screens/select_puzzle_variant_screen.dart
+++ lib/screens/select_puzzle_variant_screen.dart
@@ -4,6 +4,7 @@ import 'package:puzzlehack/cubit/audio_manager/audio_manager_cubit.dart';
 import 'package:puzzlehack/widgets/puzzle_app_bar.dart';
 import 'package:puzzlehack/widgets/puzzle_selection_card.dart';
 import 'package:puzzlehack/widgets/utils/text_theme.dart';
+import 'package:puzzlehack/widgets/utils/display_size.dart';
 
 class SelectPuzzleVariantScreen extends StatefulWidget {
   final AudioManagerCubit audioManagerCubit;
@@ -36,6 +37,8 @@ class _SelectPuzzleVariantScreenState extends State<SelectPuzzleVariantScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final windowSize = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PuzzleAppBar(
@@ -63,41 +66,47 @@ class _SelectPuzzleVariantScreenState extends State<SelectPuzzleVariantScreen> {
             ),
             Expanded(
               flex: 8,
-              child: Padding(
-                padding: const EdgeInsets.all(30.0).add(
-                    const EdgeInsets.symmetric(
-                        horizontal: 50)), //TODO adjustments
-                child: LayoutBuilder(
-                  builder: (context, constraints) {
-                    final bool isLandscape =
-                        constraints.maxWidth > constraints.maxHeight;
-                    final List<Widget> selectionCards = List.generate(
-                      PuzzleDifficulty.values.length,
-                      (index) => Expanded(
-                        child: Center(
-                          child: AspectRatio(
-                            aspectRatio: 2,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(30.0).add(
+                      const EdgeInsets.symmetric(
+                          horizontal: 50)), //TODO adjustments
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      final bool isLandscape =
+                          constraints.maxWidth > constraints.maxHeight;
+                      final List<Widget> selectionCards = List.generate(
+                        PuzzleDifficulty.values.length,
+                        (index) => SizedBox.fromSize(
+                          size: windowSize.isSmall
+                              ? const Size(200, 150)
+                              : windowSize.isLarge
+                                  ? const Size(250, 200)
+                                  : const Size(150, 100),
+                          child: Center(
                             child: PuzzleSelectionCard(
                               audioManagerCubit: widget.audioManagerCubit,
                               difficulty: PuzzleDifficulty.values[index],
                             ),
                           ),
                         ),
-                      ),
-                    );
-                    return AnimatedSwitcher(
-                      duration: const Duration(milliseconds: 50),
-                      transitionBuilder: (child, animation) =>
-                          FadeTransition(opacity: animation, child: child),
-                      child: isLandscape
-                          ? Row(
-                              children: selectionCards,
-                            )
-                          : Column(
-                              children: selectionCards,
-                            ),
-                    );
-                  },
+                      );
+                      return AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 50),
+                        transitionBuilder: (child, animation) =>
+                            FadeTransition(opacity: animation, child: child),
+                        child: isLandscape
+                            ? Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: selectionCards,
+                              )
+                            : Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: selectionCards,
+                              ),
+                      );
+                    },
+                  ),
                 ),
               ),
             )
--- lib/widgets/puzzle_selection_card.dart
+++ lib/widgets/puzzle_selection_card.dart
@@ -6,6 +6,7 @@ import 'package:puzzlehack/core/puzzle/puzzle_difficulty.dart';
 import 'package:puzzlehack/core/puzzle_logic/cubit/game_session_cubit.dart';
 import 'package:puzzlehack/cubit/audio_manager/audio_manager_cubit.dart';
 import 'package:puzzlehack/screens/game_session_page.dart';
+import 'package:puzzlehack/widgets/utils/text_theme.dart';
 
 class PuzzleSelectionCard extends StatefulWidget {
   final PuzzleDifficulty difficulty;
@@ -36,6 +37,10 @@ class _PuzzleSelectionCardState extends State<PuzzleSelectionCard> {
     setState(() {
       isHovering = true;
     });
+
+    if(widget.audioManagerCubit.state.soundsEnabled){
+      widget.audioManagerCubit.audioDataDelegate.playComponentHoveredSound();
+    }
   }
 
   void onExit(PointerEvent details) {
@@ -46,6 +51,8 @@ class _PuzzleSelectionCardState extends State<PuzzleSelectionCard> {
 
   @override
   Widget build(BuildContext context) {
+    final double cardHeight = MediaQuery.of(context).size.height;
+
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       onEnter: onEnter,
@@ -65,7 +72,7 @@ class _PuzzleSelectionCardState extends State<PuzzleSelectionCard> {
               puzzleDifficulty: widget.difficulty,
             );
 
-            widget.audioManagerCubit.audioDataDelegate.pausePreGameMusic();
+            unawaited(widget.audioManagerCubit.audioDataDelegate.pausePreGameMusic());
 
             unawaited(
               Navigator.push(
@@ -98,25 +105,24 @@ class _PuzzleSelectionCardState extends State<PuzzleSelectionCard> {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
+                  AnimatedDefaultTextStyle(
+                    duration: const Duration(milliseconds: 150),
+                    style: isHovering
+                        ? context.sizeAwareTextTheme.headline2!
+                        : context.sizeAwareTextTheme.headline3!,
+                    child: Text(
+                      "${widget.difficulty.puzzleDimension}×${widget.difficulty.puzzleDimension}",
+                    ),
+                  ),
                   AnimatedSwitcher(
                     duration: animationDuration2,
                     child: isHovering
-                        ? Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                "${widget.difficulty.puzzleDimension}×${widget.difficulty.puzzleDimension}",
-                                style: isHovering
-                                    ? Theme.of(context).textTheme.headline3
-                                    : Theme.of(context).textTheme.headline4,
-                              ),
-                              Text(
-                                "${(widget.difficulty.puzzleDimension * widget.difficulty.puzzleDimension) - 1} Tiles",
-                                style: isHovering
-                                    ? Theme.of(context).textTheme.subtitle1
-                                    : Theme.of(context).textTheme.subtitle2,
-                              ),
-                            ],
+                        ? AnimatedDefaultTextStyle(
+                            duration: const Duration(milliseconds: 150),
+                            style: context.sizeAwareTextTheme.subtitle1!,
+                            child: Text(
+                              "${(widget.difficulty.puzzleDimension * widget.difficulty.puzzleDimension) - 1} Tiles",
+                            ),
                           )
                         : const SizedBox.shrink(),
                     transitionBuilder: (child, animation) => FadeTransition(
@@ -127,24 +133,26 @@ class _PuzzleSelectionCardState extends State<PuzzleSelectionCard> {
                   AnimatedPadding(
                     duration: animationDuration1,
                     padding: isHovering
-                        ? const EdgeInsets.only(top: 30.0)
-                        : const EdgeInsets.all(0.0),
+                        ? const EdgeInsets.only(top: 20.0)
+                        : const EdgeInsets.all(10.0),
                     child: AnimatedContainer(
                       duration: animationDuration2,
                       curve: animationCurve,
                       decoration: BoxDecoration(
-                        color: isHovering
-                            ? Colors.orange
-                            : Colors.orange.withOpacity(0.6),
+                        color: isHovering ? Colors.orange[600] : Colors.orange,
                       ),
                       padding: isHovering
                           ? const EdgeInsets.symmetric(
                               horizontal: 20, vertical: 10)
                           : const EdgeInsets.symmetric(
                               horizontal: 30, vertical: 20),
-                      child: Text(
-                        describeEnum(widget.difficulty).toUpperCase(),
-                        style: Theme.of(context).textTheme.button,
+                      child: AnimatedDefaultTextStyle(
+                        duration: const Duration(milliseconds: 150),
+                        style: context.sizeAwareTextTheme.button!
+                            .copyWith(fontSize: cardHeight * 0.03),
+                        child: Text(
+                          describeEnum(widget.difficulty).toUpperCase(),
+                        ),
                       ),
                     ),
                   ),
