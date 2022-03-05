9fc8cd75259e0404068efef71445bc5b57af6368
justshreyas <professionally.shreyas@gmail.com>
2022-03-06 00:28:33 +0530

    TASK : Add Settings Button

---
+++ lib/widgets/settings_button.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:puzzlehack/cubit/audio_manager/audio_manager_cubit.dart';
+
+class PuzzleSettingsToggleButton extends StatefulWidget {
+  final AudioManagerCubit audioManagerCubit;
+  final IconData enabledIcon, disabledIcon;
+  final VoidCallback onTap;
+  final bool enabled;
+
+  const PuzzleSettingsToggleButton({
+    Key? key,
+    required this.audioManagerCubit,
+    required this.enabledIcon,
+    required this.disabledIcon,
+    required this.onTap,
+    required this.enabled,
+  }) : super(key: key);
+
+  @override
+  State<PuzzleSettingsToggleButton> createState() =>
+      _PuzzleSettingsToggleButtonState();
+}
+
+class _PuzzleSettingsToggleButtonState
+    extends State<PuzzleSettingsToggleButton> {
+  bool isHovering = false;
+  bool didChangeOnHover = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void onEnter(PointerEvent details) {
+    setState(() {
+      isHovering = true;
+    });
+
+    if (widget.audioManagerCubit.state.soundsEnabled) {
+      widget.audioManagerCubit.audioDataDelegate.playComponentHoveredSound();
+    }
+  }
+
+  void onExit(PointerEvent details) {
+    setState(() {
+      isHovering = false;
+      didChangeOnHover = false;
+    });
+  }
+
+  Color get color {
+    if (isHovering && widget.enabled) {
+      return Colors.orange[600]!;
+    } else if (widget.enabled) {
+      return Colors.orange;
+    } else if ((isHovering && !widget.enabled && !didChangeOnHover)) {
+      return Colors.orange[400]!;
+    } else {
+      return Colors.grey[400]!;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPadding(
+      duration: const Duration(milliseconds: 50),
+      padding:
+          isHovering ? const EdgeInsets.all(10.0) : const EdgeInsets.all(20.0),
+      child: MouseRegion(
+        onEnter: onEnter,
+        onExit: onExit,
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              didChangeOnHover = true;
+            });
+            widget.onTap();
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: color,
+                width: 2,
+              ),
+            ),
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Center(
+                child: Icon(
+                  widget.enabled ? widget.enabledIcon : widget.disabledIcon,
+                  color: color,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
