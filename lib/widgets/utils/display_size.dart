3225a4d4f7c61177e3075494e08cc2a7a8185dbc
justshreyas <professionally.shreyas@gmail.com>
2022-03-12 00:52:07 +0530

    FIX : Display size breakpoint

--- lib/widgets/utils/display_size.dart
+++ lib/widgets/utils/display_size.dart
@@ -7,8 +7,8 @@ enum DisplaySize {
 }
 
 extension DisplaySizeUtil on Size {
-  bool get isLarge => shortestSide >= 750;
-  bool get isMedium => shortestSide > 350 && shortestSide < 750;
+  bool get isLarge => shortestSide >= 650;
+  bool get isMedium => shortestSide > 350 && shortestSide < 650;
   bool get isSmall => shortestSide <= 350;
 
   DisplaySize get displaySize {
